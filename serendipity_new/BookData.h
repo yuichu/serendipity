372bd89ba61a9a8697a34d7894fb62457cc23748
yuibun <yuuibun@gmail.com>
2018-05-01 14:46:29 -0700

    initialized a linked list in main and manually added books with pointers

--- serendipity_new/BookData.h
+++ serendipity_new/BookData.h
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <iomanip>
 using namespace std;
 
 class BookData
@@ -10,7 +12,7 @@ class BookData
 public:
 	BookData();
 	BookData(string bookTitle, string isbn, string author, string publisher);
-	~BookData();
+	~BookData(void);
 
 	virtual void print();
 
@@ -26,6 +28,18 @@ public:
 
 	void removeBook();
 
+	bool operator>=(const BookData& other_Book) const;
+	bool operator==(const BookData& other_Book) const;
+	bool operator!=(const BookData& other_Book) const;
+
+	friend ostream& operator<< (ostream& osObject, const BookData& c)
+	{
+		osObject << left << setfill(' ')<< " " << setw(29) << c.bookTitle.substr (0,28)
+			<< setw(11) << c.isbn << setw(15) << c.author.substr(0,14)
+			<< setw(11) << c.publisher.substr (0,14) << endl;
+
+		return osObject;
+	}
 
 private:
 	string bookTitle;
@@ -34,5 +48,206 @@ private:
 	string publisher;
 
 };
+
+//															CONSTRUCTORS
+//----------------------------------------------------------------------
+// Function: BookData()
+// default constructor
+// Receives: none
+// Returns: none
+//----------------------------------------------------------------------
+BookData::BookData()
+{
+	setTitle("Not Set");
+	setIsbn("Not Set");
+	setAuthor("Not Set");
+	setPub("Not Set");
+}
+
+//----------------------------------------------------------------------
+// Function: BookData()
+// overload constructor
+// Receives: string bookTitle, string isbn, string author, string publisher,
+//			string dateAdded, int qtyOnHand, double wholesale, double retail
+// Returns: none
+//----------------------------------------------------------------------
+BookData::BookData(string bookTitle, string isbn, string author, string publisher)
+{
+	setTitle(bookTitle);
+	setIsbn(isbn);
+	setAuthor(author);
+	setPub(publisher);
+}
+
+
+//----------------------------------------------------------------------
+// Function: ~BookData()
+// deconstructor
+// Receives: none
+// Returns: none
+//----------------------------------------------------------------------
+BookData::~BookData(){}
+
+
+//																PRINT
+//----------------------------------------------------------------------
+// Function: print()
+// print book info
+// Receives: none
+// Returns: none
+//----------------------------------------------------------------------
+void BookData::print()
+{
+	cout << "Title: " << getTitle() << "\n"
+			<< "ISBN: " << getIsbn() << "\n"
+			<< "Author: " << getAuthor() << "\n"
+			<< "Publisher: " << getPub() << "\n";
+}
+
+//																SETTERS
+//----------------------------------------------------------------------
+// Function: setTitle()
+// set book title
+// Receives: string bookTitle
+// Returns: none
+//----------------------------------------------------------------------
+void BookData::setTitle(string bookTitle)
+{
+	this->bookTitle = bookTitle;
+}
+
+//----------------------------------------------------------------------
+// Function: setIsbn()
+// set book ISBN
+// Receives: string isbn
+// Returns: none
+//----------------------------------------------------------------------
+void BookData::setIsbn(string isbn)
+{
+	this->isbn = isbn;
+}
+
+//----------------------------------------------------------------------
+// Function: setAuthor()
+// set book author
+// Receives: string author
+// Returns: none
+//----------------------------------------------------------------------
+void BookData::setAuthor(string author)
+{
+	this->author = author;
+}
+
+//----------------------------------------------------------------------
+// Function: setPub()
+// set book publisher
+// Receives: string publisher
+// Returns: none
+//----------------------------------------------------------------------
+void BookData::setPub(string publisher)
+{
+	this->publisher = publisher;
+}
+
+//																GETTERS
+//----------------------------------------------------------------------
+// Function: getTitle()
+// get book title
+// Receives: none
+// Returns: string bookTitle
+//----------------------------------------------------------------------
+string BookData::getTitle() const
+{
+	return bookTitle;
+}
+
+//----------------------------------------------------------------------
+// Function: getIsbn()
+// get book ISBN
+// Receives: none
+// Returns: string isbn
+//----------------------------------------------------------------------
+string BookData::getIsbn() const
+{
+	return isbn;
+}
+
+//----------------------------------------------------------------------
+// Function: getAuthor()
+// get book author
+// Receives: none
+// Returns: string author
+//----------------------------------------------------------------------
+string BookData::getAuthor() const
+{
+	return author;
+}
+
+//----------------------------------------------------------------------
+// Function: getPub()
+// get book publisher
+// Receives: none
+// Returns: string publisher
+//----------------------------------------------------------------------
+string BookData::getPub() const
+{
+	return publisher;
+}
+
+//														OVERLOAD OPERATOR
+//----------------------------------------------------------------------
+bool BookData::operator>=(const BookData& other_Book) const
+{
+	bool found = false;
+
+	if (this != &other_Book)
+	{
+		if (this != &other_Book)
+		{
+			if((strcmp(this->isbn.c_str(), other_Book.isbn.c_str())) >= 0)
+			{
+				found = true;
+			}
+		}
+	}
+	return found;
+}
+
+//----------------------------------------------------------------------
+bool BookData::operator==(const BookData& other_Book) const
+{
+	bool found = false;
+	if(this != &other_Book)
+	{
+		if (this != &other_Book)
+		{
+			if ((strcmp(this->isbn.c_str(), other_Book.isbn.c_str())) == 0)
+			{
+				found = true;
+			}
+
+		}
+	}
+	return found;
+}
+
+//----------------------------------------------------------------------
+bool BookData::operator!=(const BookData& other_Book) const
+{
+	bool found = false;
+	if(this != &other_Book)
+	{
+		if (this != &other_Book)
+		{
+			if ((strcmp(this->isbn.c_str(), other_Book.isbn.c_str())) == 0)
+			{
+				found = true;
+			}
+		}
+	}
+	return !found;
+}
+
+//----------------------------------------------------------------------
 
 #endif /* BOOKDATA_H_ */
--- serendipity_new/main.cpp
+++ serendipity_new/main.cpp
@@ -48,6 +48,29 @@ const int DBSIZE = 1;
 int main()
 {
 
+	orderedLinkedList<InventoryBook> list1; //create linked list
+	list1.initializeList(); // initialize list
 
+	InventoryBook *book1 = new InventoryBook ("Star Wars", "0345260791", "George Lucas", "Del Rey", "10/18/2017", 5, 59.95, 100.00);
+	InventoryBook *book2 = new InventoryBook ("The Empire Strikes Back (Star Wars)", "034529209X", "George Lucas", "Ballantine", "04/01/2017", 2, 179.58, 185.00);
+	InventoryBook *book3 = new InventoryBook ("Splinter of the Mind's Eye (Star Wars)", "0345320239", "George Lucas", "Del Rey", "11/08/2017", 3, 24, 26.50);
+	InventoryBook *book4 = new InventoryBook ("Return of the Jedi (Star Wars)", "0345307674", "George Lucas", "Del Rey", "10/09/2017", 4, 2.94, 6.50);
+	InventoryBook *book5 = new InventoryBook ("Heir to the Empire (Star Wars: The Thrawn Trilogy, Vol. 1)", "0553296124", "Timothy Zahn", "Bantam", "11/01/2017", 10, 25.38, 27.00);
+
+	// insert books into the linked list
+	list1.insert(*book1);
+	list1.insert(*book2);
+	list1.insert(*book3);
+	list1.insert(*book4);
+	list1.insert(*book5);
+
+	linkedListIterator<InventoryBook> myIterator; // create an iterator to iterate through the linked list
+
+	for (myIterator = list1.begin(); myIterator != list1.end(); ++myIterator)
+	{
+		cout << (*myIterator).getTitle() << endl;
+	}
+
+	system("pause");
 	return 0;
 }
